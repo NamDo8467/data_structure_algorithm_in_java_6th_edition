d780d450fc0aa812e4109d7cf8656b62922c9b6d
NamDo8467 <huynhnamdo@gmail.com>
2024-02-21 23:17:44 -0500

    finish R-3.2

--- src/ch3/R_3_2.java
+++ src/ch3/R_3_2.java
@@ -1,10 +1,35 @@
 package ch3;
-
+import java.util.Random;
+import java.util.Arrays;
 public class R_3_2 {
-
+	public static void randomRemove(int [] array) {
+		int length = array.length;
+		boolean terminate = false;
+		Random random = new Random();
+		int r = 0;
+		while(terminate == false) {
+			r = random.nextInt(0, length);
+			array[r] = 0;
+			for(int i = 0; i<array.length;i++){
+				if(array[i] != 0) {
+					terminate = false;
+					break;
+				}else {
+					terminate = true;
+				}
+			}
+			
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] array = {1,2,3,4,5};
+		System.out.println("Before randomRemove: ");
+		System.out.println(Arrays.toString(array));
+		randomRemove(array);
+		System.out.println();
+		System.out.println("After randomRemove: ");
+		System.out.println(Arrays.toString(array));
 	}
 
 }
