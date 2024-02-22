f8e5c2d641999589760e55a63eacfde7bfeb3f2e
NamDo8467 <huynhnamdo@gmail.com>
2024-02-21 22:25:05 -0500

    initialized project

---
+++ src/data_structure_algorithm_in_java_6th_edition/Main.java
@@ -0,0 +1,10 @@
+package data_structure_algorithm_in_java_6th_edition;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
