550e03602e64d171f810a0e1c3d03121ec492c4c
NamDo8467 <huynhnamdo@gmail.com>
2024-02-21 23:57:51 -0500

    finished R-3.3

---
+++ src/ch3/R_3_3.java
@@ -0,0 +1,17 @@
+package ch3;
+
+public class R_3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String answerString = "To perform the Caesar cipher for messages that are written in an alphabet-based language other than English, such as Greek,\n"
+				+ "Russian, or Hebrew, you need to have an additional parameter called 'language' to the constructor of CaesarCipher. After that, \nyou will have to incldue "
+				+ "if-statements to check what unicode should be used for a corresponding language.\n\n"
+				+ "Link to check: \n"
+				+ " - https://symbl.cc/en/alphabets/english/\n"
+				+ " - https://www.ssec.wisc.edu/~tomw/java/unicode.html";
+		
+		System.out.println(answerString);
+	}
+
+}
